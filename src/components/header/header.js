54df7680aea153e32355d072f4439200fed04c3c
Linda Ramos <ltramos7@gmail.com>
2020-12-20 18:01:31 -0600

    added components that build the basic divs in the app

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+r
+
+class  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export de
+
+fault ;
