7211efba0591532ddaed3252162de9aa1b71f20b
Linda Ramos <ltramos7@gmail.com>
2020-12-20 18:24:22 -0600

    added a basic search bar and search button. No functionality yet.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import React from 'react'
 import Header from './components/headerSection/HeaderSection'
+import SearchArea from './components/searchArea/searchArea'
 
 function App() {
   return (
     //only components should live here. Most logic will live in the imported components
     <div>
       <Header/>
+      <SearchArea/>
     </div>
   );
 }
---
+++ src/components/searchArea/searchArea.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class searchArea extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input placeholder="Search here..."></input>
+                    <button>Search</button>
+                </form>
+            </div>
+        )
+    }
+}
