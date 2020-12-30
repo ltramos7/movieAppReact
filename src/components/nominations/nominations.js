abe8f4a52b1f770435766e93f47894eef3e9b4ac
Linda Ramos <ltramos7@gmail.com>
2020-12-30 14:54:57 -0600

    added remove button to movies in nominated list.

--- src/components/nominations/nominations.js
+++ src/components/nominations/nominations.js
@@ -6,9 +6,13 @@ export default class nominations extends Component {
     renderNominatedMovies = () => {
         let nominatedMovies = this.props.nominatedMovie
         return nominatedMovies.map(movie => {
-            return <p key={movie.imdbID}>{movie.Title} ({movie.Year})</p>
+            return (
+                <div>
+                    <p key={movie.imdbID}>{movie.Title} ({movie.Year})</p>
+                    <button>Remove</button>
+                </div>
+            )
         }) 
-
     }
 
     render() {
